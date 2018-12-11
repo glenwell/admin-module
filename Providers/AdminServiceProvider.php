51d1ae803816801985a3077f9afe3785ddf96eb9
Josh <dannsj@gmail.com>
2018-12-11 09:57:02 +0300

    More tests

--- Providers/AdminServiceProvider.php
+++ Providers/AdminServiceProvider.php
@@ -36,6 +36,7 @@ class AdminServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->register(RouteServiceProvider::class);
+        $this->app->register(VoyagerServiceProvider::class);
     }
 
     /**
