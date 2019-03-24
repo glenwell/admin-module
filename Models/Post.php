7e2e3a61e1a90d173610793cd31e3892fd5e9f1f
Josh <dannsj@glenwell.com>
2019-03-24 09:37:21 +0300

    Fixed problem with tcg voyager

--- Models/Post.php
+++ Models/Post.php
@@ -3,9 +3,15 @@
 namespace Modules\Admin\Models;
 
 use TCG\Voyager\Traits\Resizable;
+use TCG\Voyager\Facades\Voyager;
 
 class Post extends \TCG\Voyager\Models\Post
 {
     use Resizable;
+
+    public function author()
+    {
+        return $this->belongsTo(Voyager::modelClass('User'), 'author_id', 'id');
+    }
 
 }
